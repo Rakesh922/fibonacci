83d3aaafa678e61e670e71e220d96d55aa693fbc
Rakesh922 <102220582+Rakesh922@users.noreply.github.com>
2023-01-03 15:34:25 +0530

    Add files via upload

---
+++ fibanocci.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import fibanocci
+
+
+# In[2]:
+
+
+fibanocci.fib()
+
+
+# In[3]:
+
+
+import fibanocci
+
+
+# In[4]:
+
+
+fibanocci.fib()
+
+
+# In[5]:
+
+
+import fibanocci
+
+
+# In[6]:
+
+
+fibanocci.fib()
+
+
+# In[7]:
+
+
+import fibanocci
+
+
+# In[8]:
+
+
+fibanocci.fib()
+
